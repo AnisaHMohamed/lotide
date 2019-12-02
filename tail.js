df54b08516e9d065b3593f00e086015598e729a1
Anisa <anisahamohamed@gmail.com>
2019-12-02 16:38:15 +0000

    added tailTest.js

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log("🚨  🚨  🚨 🚨Assertion Failed: " + actual + " != " + expected);
-  } else {
-    console.log(`🎊  🎉  🎊  🎉Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+
 const tail = function(list) {
   let last = [...list];
   if (last.length <= 1) {
@@ -14,19 +8,4 @@ const tail = function(list) {
 
   return last;
 };
-//  Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// // Test Case 2: ...
-// // length is 1 
-const words = tail(["hello"]);
-assertEqual(words.length, 0);
-
-
-// // Test Case 3: ...
-// // length is 0 
-const word = tail([]);
-assertEqual(word.length, 0);
+module.exports = tail
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,4 @@
 const assertEqual = require("../assertEqual.js")
-// TEST CODE
 
-//assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
-//assertEqual(1, 11);
